301246c8bf913154408e784e8d6fbce938a2b13e
alessandrogammieri <alessandrogammieri.it@gmail.com>
2019-08-28 15:09:36 +0200

    initialized html + css + js

---
+++ script.js
@@ -0,0 +1,65 @@
+
+function init() {
+  // Popoliamo la lista
+  getTodoList();
+  // Cancelliamo l'elemento con un click
+  $(document).on("click", "p.delete", deleteList);
+  // Aggiungiamo l'elemento in lista con un click
+  $("#submit").click();
+}
+
+// Funzione che ci permette di cancellare gli elementi in lista
+function deleteList() {
+  var elemento = $(this);
+  var box = elemento.parent();
+  var theid = box.data("id")
+  // Chiamata AJAX per rimuovere un oggetto in base all'attributo [data-id]
+  $.ajax({
+    url: "http://157.230.17.132:3009/todos/" + theid,
+    method: "DELETE",
+    success: function () {
+      box.remove();
+    },
+    error: function () {
+      alert("C'è stato un errore nella cancellazione");
+    }
+  })
+}
+
+// Funzione che ci permette di popolare la lista
+function getTodoList() {
+  // Chiamata AJAX per popolare la lista
+  $.ajax({
+    url: "http://157.230.17.132:3009/todos",
+    method: "GET",
+    success: function (data) {
+      printList(data);
+    },
+    error: function () {
+      alert("C'è stato un errore in download");
+    }
+  })
+}
+
+// Funzione che ci permette di stampare la lista
+function printList(list) {
+  // Dichiariamo una variabile con i riferimenti del contenitore da popolare
+  var contenitore = $(".container");
+  // Creo il clone dell'elemento con Handlebars
+  var source = $("#item-template").html();
+  var template = Handlebars.compile(source);
+  // Ciclo for per generare gli oggetti della chiamata in base alla lunghezza della lista
+  for (var i = 0; i < list.length; i++) {
+    var item = list[i];
+    // Dichiariamo in una variabile tutti i segnaposto
+    var context = {
+    text: item.text,
+    id: item.id
+    };
+    // Stampo la lista
+    var html = template(context);
+    contenitore.append(html);
+  }
+}
+
+$(document).ready(init);
